2fd83be1c6848500ae540ba4624228530fb5b961
moe moe <moe moe>
2017-05-11 11:10:59 -0700

    fixing volume calculator

--- ConeVolumeCalculator.py
+++ ConeVolumeCalculator.py
@@ -7,6 +7,12 @@
 # Define Function below
 # be sure to return an integer
 
+import math
+
+def calculateConeVolume(radius, height):
+    volume=(1/3)*height*math.pi*radius**2
+    volume=round(volume,2)
+    return volume
 
 if __name__ == '__main__':
     # Call the function in here if you want to test it
